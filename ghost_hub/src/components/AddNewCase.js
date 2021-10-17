ea8de651d3c1aa70786a8b2cff256f9d80ceeb3b
shadowbattalion <hafizspaceumar@gmail.com>
2021-10-17 16:26:16 +0000

    Doing add cases for encounter

--- ghost_hub/src/components/AddNewCase.js
+++ ghost_hub/src/components/AddNewCase.js
@@ -5,18 +5,103 @@ export default class AddNewCase extends React.Component {
     url_api = ""
 
     state = {
-        'data': [
-           
-        ]
+       "encounters":[
+                       
+                   ],
+        new_sightings_description:""
+
+   }
+
+
+   delete_encounter = (temp_id) =>{
+
+     let encounters_list=this.state.encounters
+
+     let index_to_delete = encounters_list.indexOf(temp_id)
+
+     let new_encounter_list=[...encounters_list.slice(0,index_to_delete), ...encounters_list.slice(index_to_delete+1)]
+
+     this.setState({
+
+        "encounters":new_encounter_list
+
+     })
+
+
+
+   }
+
+
+    display_added_encounters(){
+
+        let encounter_jsx=[]
+
+        let i=0
+        for(let encounter of this.state.encounters){
+            let each_encounter = (
+                <React.Fragment key={encounter.temp_id}>
+                    <div>    
+                        {encounter.sightings_description}
+                        <button className="btn btn-danger btn-sm mx-1" onClick={()=>{this.delete_encounter(encounter.temp_id)}}>Delete</button>
+                    </div>
+                </React.Fragment>)
+
+            encounter_jsx.push(each_encounter)
+            i++
+        }
+
+
+
+        return encounter_jsx
+
     }
 
 
+    update_encounter_field=(e)=>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
+    add_encounter=()=>{
+
+        let new_encounter={
+            "id":"front_end_id"+,
+            "sightings_description":this.state.new_sightings_description
+
+        }
+        
+        this.state.new_sightings_description=""
+        
+        this.setState({
+            'encounters': [...this.state.encounters, new_encounter]
+        })
+    }
+    
+    display_form(){
+
+        return(
+            <React.Fragment>
+                <label>Description</label>
+                <input type="text" name="new_sightings_description" className="form-control" value={this.state.new_sightings_description} onChange={this.update_encounter_field} />
+                <button className="btn btn-success btn-sm" onClick={this.add_encounter}>Add!</button>
+
+
+            </React.Fragment>
+
+        )
+
+
+
+
+    }
 
 
     render() {
         return (<React.Fragment>
             <h1>Add a Case</h1>
+            {this.display_added_encounters()}
+            {this.display_form()}
         </React.Fragment>)
     }
 
