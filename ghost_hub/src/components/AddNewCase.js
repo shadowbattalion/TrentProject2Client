b48719c6f5afc3fb7b8a5956b0abbd9e2fa3d03e
shadowbattalion <hafizspaceumar@gmail.com>
2021-10-18 06:56:23 +0000

    edit encounter done

--- ghost_hub/src/components/AddNewCase.js
+++ ghost_hub/src/components/AddNewCase.js
@@ -9,7 +9,9 @@ export default class AddNewCase extends React.Component {
                        
                    ],
         new_sightings_description:"",
-        edit_mode:{},
+        edit_mode:{
+            "_id":0
+        },
         edit_sightings_description:""
 
    }
@@ -67,25 +69,32 @@ export default class AddNewCase extends React.Component {
 
       }
 
+      
 
-    // edit_counter = (encounter_edit) =>{
-
-
-    //     let encounters_list=this.state.encounters
-   
-    //     let index_to_delete = encounters_list.findIndex(encounter=>encounter._id==encounter_delete._id)
-   
-    //     let new_encounter_list=[...encounters_list.slice(0,index_to_delete), ...encounters_list.slice(index_to_delete+1)]
-   
-    //     this.setState({
-   
-    //        "encounters":new_encounter_list
-   
-    //     })
+    edit_encounter = () =>{
 
 
+        let edited_encounter = {
+            
+            "_id":this.state.edit_mode._id,
+            "sightings_description":this.state.edit_sightings_description
+        }
+        
+        let index_to_edit = this.state.encounters.findIndex( encounter => encounter._id == edited_encounter._id);
+        
+        
+        let updated_encounters = [...this.state.encounters.slice(0, index_to_edit), edited_encounter, ...this.state.encounters.slice(index_to_edit+1)]
 
-    // }
+        this.setState({
+            "encounters": updated_encounters,
+            'edit_mode':{
+                '_id':0
+            }
+        })
+
+
+
+    }
 
 
 
@@ -103,16 +112,16 @@ export default class AddNewCase extends React.Component {
 
             let each_encounter=""
 
-            if(encounter._id!=this.state.edit_mode._id || this.state.edit_mode._id!={}){
+            if(encounter._id==this.state.edit_mode._id && this.state.edit_mode._id != 0){
 
                 each_encounter = (
                     <React.Fragment key={encounter._id}>
                         <div>    
-                        {this.display_edit_form()}
+                            {this.display_edit_form()}
                         </div>
                     </React.Fragment>)
-
-            }else{
+                
+            } else{
 
                 each_encounter = (
                     <React.Fragment key={encounter._id}>
@@ -122,6 +131,8 @@ export default class AddNewCase extends React.Component {
                             <button className="btn btn-danger btn-sm mx-1" onClick={()=>{this.delete_encounter(encounter)}}>Delete</button>
                         </div>
                     </React.Fragment>)
+
+
             }
 
             encounter_jsx.push(each_encounter)
@@ -142,7 +153,7 @@ export default class AddNewCase extends React.Component {
             <React.Fragment>
                     <label>Description</label>
                     <input type="text" name="edit_sightings_description" className="form-control" value={this.state.edit_sightings_description} onChange={this.update_encounter_field} />
-                    {/* <button className="btn btn-success btn-sm" onClick={this.add_encounter}>Add!</button> */}
+                    <button className="btn btn-success btn-sm" onClick={this.edit_encounter}>Done!</button>
 
 
             </React.Fragment>
